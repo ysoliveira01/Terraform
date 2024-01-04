e6fd4bc4ce9ca2c62504950c4476efed2d5df97c
ysoliveira01 <ysoliveira01@gmail.com>
2024-01-04 10:15:37 -0300

    adicionando rbac no cluster

--- Azure/main.tf
+++ Azure/main.tf
@@ -8,6 +8,16 @@ resource "azurerm_kubernetes_cluster" "lab" {
   location            = var.region
   resource_group_name = azurerm_resource_group.lab-aks.name
   dns_prefix          = "exampleaks1"
+  private_cluster_enabled = true
+
+  role_based_access_control {
+    enabled = true
+  
+    azure_active_directory {
+      managed = true
+      azure_rbac_enable = true
+    }
+  }
 
   default_node_pool {
     name       = "worker"
@@ -31,4 +41,24 @@ resource "azurerm_kubernetes_cluster" "lab" {
   #   client_id = ""
   #   client_secret = ""
   # }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = true
+    }
+  }
+
+  network_profile {
+    load_balancer_sku  = "standard"
+    network_plugin     = "kubenet"
+    network_policy     = "calico"
+    docker_bridge_cidr = "172.17.0.1/16"
+    service_cidr       = "192.168.0.0/16"
+    dns_service_ip     = "192.168.0.10"
+   }
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
